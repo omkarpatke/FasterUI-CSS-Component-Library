3e8f256ddf6cb404a2536f73118c3374f02ffe5a
omkar patke <omkarpatke1101@gmail.com>
2022-02-06 17:47:52 +0530

    style : added responsive btn funtionallity

--- components.js
+++ components.js
@@ -1,4 +1,26 @@
-const barBtn = document.querySelector('.bars');
+const barsBtn = document.querySelector('.bars');
+const sideBar = document.querySelector('.sideBar');
+const closeBtn = document.querySelector('.close-btn');
+sideBar.classList.add('hidden');
+closeBtn.classList.add('hidden');
 
+barsBtn.addEventListener('click',() => {
+    console.log('clicked');
+    sideBar.classList.remove('hidden');
+    barsBtn.style.display = 'none';
+    closeBtn.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click' , () => {
+    closeBtn.classList.add('hidden');
+    sideBar.classList.add('hidden');
+    barsBtn.style.display = 'block';
+})
+
+sideBar.addEventListener('click' , () => {
+    closeBtn.classList.add('hidden');
+    sideBar.classList.add('hidden');
+    barsBtn.style.display = 'block';
+})
 
 
