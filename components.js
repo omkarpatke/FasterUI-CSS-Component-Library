d39dc3e674eb931362db98ec4c81b36ed27deca1
omkar patke <omkarpatke1101@gmail.com>
2022-02-06 18:23:19 +0530

    docs : added docs link on home page

--- components.js
+++ components.js
@@ -1,10 +1,12 @@
 const barsBtn = document.querySelector('.bars');
 const sideBar = document.querySelector('.sideBar');
 const closeBtn = document.querySelector('.close-btn');
+const docsBtn = document.querySelector('.res-doc-link');
 closeBtn.classList.add('hidden');
 
 if(screen.width < 500){
     sideBar.classList.add('hidden');
+    docsBtn.classList.remove('hidden')
     sideBar.addEventListener('click' , () => {
         barsBtn.style.display = 'block';
         sideBar.classList.add('hidden');
@@ -14,6 +16,7 @@ if(screen.width < 500){
 
 if(screen.width > 500){
     sideBar.classList.remove('hidden');
+    docsBtn.classList.add('hidden')
 }
 
 barsBtn.addEventListener('click',() => {
